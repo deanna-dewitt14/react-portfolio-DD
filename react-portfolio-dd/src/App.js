54adc91f6dee9b1c0b8361f69b7a0b35b681c0d4
dewittd14 <deanna.dewitt14@gmail.com>
2023-01-15 13:43:25 -0500

    added functionality to app.js

--- react-portfolio-dd/src/App.js
+++ react-portfolio-dd/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import { Helmet } from 'react-helmet';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentTab, handleTabChange] = useState('about');
+  const renderTab = () => {
+    if (currentTab === 'About') { return <About /> }
+    if (currentTab === 'Contact') { return <Contact /> }
+    if (currentTab === 'Portfolio') { return <Portfolio /> }
+    if (currentTab === 'Resume') { return <Resume /> }
+    return <About />;
+  };
+
+    return (
+      <>
+      <Helmet>
+        <title> Deanna DeWitt's Portfolio | {currentTab} </title>
+      </Helmet>
+      <Header currentTab={currentTab} handleTabChange={handleTabChange}></Header>
+      <main>{renderTab()}</main>
+      <Footer></Footer>
+    </>
+    );
 }
 
 export default App;
