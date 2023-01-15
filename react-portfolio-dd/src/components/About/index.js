29473365342c2bdbf81ea48b9f6b394bd359d72f
dewittd14 <deanna.dewitt14@gmail.com>
2023-01-15 18:12:18 -0500

    added about me section

--- react-portfolio-dd/src/components/About/index.js
+++ react-portfolio-dd/src/components/About/index.js
@@ -15,7 +15,7 @@ function About() {
             <img src={profileImage} style={{ width: "100%" }} alt='profile' />
           </div>
           <p>
-            TBD
+            Hello, my name is Deanna and I am new to the world of coding. I have just graduated from UNC Chapel Hills coding bootcamp. I am excited to learn and grow in this field and am eager to take on new challenges. I am open to learning any programming language, and I am determined to become a skilled developer. I am a quick learner and am always looking for ways to improve my skills. I am looking forward to meeting other developers and learning from their experiences. I believe that with hard work and dedication, I can achieve my goals in the field of coding.
           </p>
         </div>
       </div>
